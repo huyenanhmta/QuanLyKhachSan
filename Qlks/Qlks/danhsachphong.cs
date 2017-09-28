7b2d1d4a85051f0ac675c3b44f2c9daa44d6c8bf
HoaNgomta <32055817+HoaNgomta@users.noreply.github.com>
2017-09-28 11:03:20 -0700

    danh sach phong

--- Qlks/Qlks/danhsachphong.cs
+++ Qlks/Qlks/danhsachphong.cs
@@ -17,5 +17,62 @@ namespace Qlks
         {
             InitializeComponent();
         }
+        CauLenh cl = new CauLenh();
+        DataTable dt = new DataTable();
+        private void load()
+        {
+            dt.Clear();
+            dt = cl.danhsachp(0);
+            dg.DataSource = dt;
+            rd_kt.Checked = false;
+            rd_t.Checked = false;
+            bt_loc.Enabled = false;
+            bt_xoa.Enabled = false;
+        }
+        private void danhsachphong_Load(object sender, EventArgs e)
+        {
+            load();
+        }
+
+        private void bt_loc_Click(object sender, EventArgs e)
+        {
+
+            if(rd_t.Checked == true)
+            {
+                dt.Clear();
+                dt = cl.danhsachpt(0);
+                dg.DataSource = dt;
+                bt_xoa.Enabled = true;
+            }
+            if(rd_kt.Checked == true)
+            {
+                dt.Clear();
+                dt = cl.danhsachpkt(0);
+                dg.DataSource = dt;
+                bt_xoa.Enabled = true;
+            }
+            else
+            {
+                
+            }
+        }
+
+        private void bt_xoa_Click(object sender, EventArgs e)
+        {
+            load();
+        }
+
+        private void rd_t_Click(object sender, EventArgs e)
+        {
+            bt_loc.Enabled = true;
+            bt_xoa.Enabled = false;
+            
+        }
+
+        private void rd_kt_Click(object sender, EventArgs e)
+        {
+            bt_loc.Enabled = true;
+            bt_xoa.Enabled = false;
+        }
     }
 }
